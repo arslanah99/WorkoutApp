7a2894fe301320f193c267999bbb46658f25255a
arslanah99 <Arah09@hotmail.com>
2019-06-14 14:53:26 -0600

    added dependency to navigate between pages

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/components/login/Login'
-
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <Text>LOGIN PAGE</Text>
     </View>
   );
 }
@@ -14,8 +12,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#67809f'
   },
 });
