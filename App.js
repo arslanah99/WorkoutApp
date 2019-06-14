f86bf5f212742aa0bfa5db48d958d2ee8dce147e
arslanah99 <Arah09@hotmail.com>
2019-06-14 15:46:59 -0600

    Working buttons in login page with alerts

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/components/login/Login'
-
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.welcome}>Login to IMPERIUM</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="Username"
+      />
+      <TextInput 
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.userbtn} onPress={() => alert("Login works")}>
+          <Text style={styles.btnTxt}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.userbtn} onPress={() => alert("password works")}>
+          <Text style={styles.btnTxt}>Signup</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,8 +29,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f62459',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+    color: '#fff',
+  },
+  input: {
+    width: '90%',
+    backgroundColor: "#fff",
+    padding: 15,
+    marginBottom: 10
+  },
+  userbtn: {
+    backgroundColor: "#fff68f",
+    padding: 15,
+    width: "45%"
+  },
+  btnTxt: {
+    fontSize: 18,
+    textAlign: "center"
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%"
+  }
 });
