6d8538c1307b1e16304cf3df09b3431e0a6b5ff7
arslanah99 <Arah09@hotmail.com>
2019-06-24 14:38:06 -0600

    cleaned up pages

--- App.js
+++ App.js
@@ -1,109 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, StatusBar } from 'react-native';
-import { createStackNavigator, createAppContainer, createDrawerNavigator } from 'react-navigation';
-import LoginPage from './src/components/LoginPage';
-import SignupPage from './src/components/SignupPage';
-import DashboardPage from './src/components/Dashboard';
-
-// class DetailsScreen extends React.Component {
-//   static navigationOptions = {
-//     title: 'IMPERIUM',
-//     headerRight: <View />
-// }
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Details Screen</Text>
-//       </View>
-//     );
-//   }
-// }
+import React from "react";
+import { createAppContainer, createDrawerNavigator } from "react-navigation";
+import LoginPage from "./src/components/LoginPage";
+import SignupPage from "./src/components/SignupPage";
+import DashboardPage from "./src/components/Dashboard";
 
 const MyDrawerNavigator = createDrawerNavigator({
-  Login:{
-    screen:LoginPage
-  }, 
+  Login: {
+    screen: LoginPage
+  },
   Signup: {
-    screen:SignupPage
+    screen: SignupPage
   },
   Details: {
-    screen:DashboardPage
-  }
-})
-
-const RootStack = createStackNavigator(
-  {
-    Home: LoginPage,
-    Details: DashboardPage,
-    Signup: SignupPage
-  },
-  {
-    initialRouteName: 'Home',
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "#f62459"
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        textAlign: 'center',
-        flex: 1
-      }
-    }
-  },
-
-)
-
-const MyApp = createAppContainer(MyDrawerNavigator);
-const AppContainer = createAppContainer(RootStack);
-
-type Props = {};
-export default class App extends React.Component{
-  render(){
-    return(
-      <AppContainer />,
-      <MyApp />
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f62459',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  welcome: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
-    color: '#fff',
-  },
-  input: {
-    width: '90%',
-    backgroundColor: "#fff",
-    padding: 15,
-    marginBottom: 10
-  },
-  userbtn: {
-    backgroundColor: "#fff68f",
-    padding: 15,
-    width: "45%"
-  },
-  btnTxt: {
-    fontSize: 18,
-    textAlign: "center"
-  },
-  btnContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "90%"
-  },
-  inputSignup: {
-      margin: 15,
-      height: 40,
-      borderColor: 'black',
-      borderWidth: 1,
-      width: '75%'
+    screen: DashboardPage
   }
 });
+
+const MyApp = createAppContainer(MyDrawerNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <MyApp />;
+  }
+}
